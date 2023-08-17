05db61307d32474deb636fc9a31301d7b32b4a7c
PhongNhat43 <phongnhat43@gmail.com>
2023-08-17 15:48:41 +0700

    Fix the height of label collection cell

---
+++ BacSiGanNha/BacSiGanNha/Cells/Intro/IntroImageCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  IntroImageCollectionViewCell.swift
+//  BacSiGanNha
+//
+//  Created by devsenior on 17/08/2023.
+//
+
+import UIKit
+
+class IntroImageCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
