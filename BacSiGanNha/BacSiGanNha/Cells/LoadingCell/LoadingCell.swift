02e05ec1b35a5f911ed8d69cf73b507af558dfb3
PhongNhat43 <phongnhat43@gmail.com>
2023-08-24 11:27:20 +0700

    Add pull to refresh and load more

---
+++ BacSiGanNha/BacSiGanNha/Cells/LoadingCell/LoadingCell.swift
@@ -0,0 +1,23 @@
+//
+//  LoadingCell.swift
+//  BacSiGanNha
+//
+//  Created by devsenior on 24/08/2023.
+//
+
+import UIKit
+
+class LoadingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
